47d02cb6d48b6cf7fcad51378ff022c8bc97be9c
saravana5632 <oatktg.saravana5632@gmail.com>
2026-09-19 19:03:30 +0530

    Added Buzz number

---
+++ BuzzNumber.java
@@ -0,0 +1,11 @@
+package numberproblems;
+
+public class BuzzNumber {
+public static void main(String[] args) {
+	int a =47;
+	if(a%10 == 7 || a%7 == 0)
+		System.out.println("Buzz");
+	else 
+		System.out.println("Not Buzz");
+}
+}
