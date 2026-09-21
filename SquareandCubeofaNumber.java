4e83b620a75de815bef0c11bc84bc3b2874511de
saravana5632 <oatktg.saravana5632@gmail.com>
2026-09-21 23:01:07 +0530

    Added Square and cube of a number

---
+++ SquareandCubeofaNumber.java
@@ -0,0 +1,9 @@
+package numberproblems;
+
+public class SquareandCubeofaNumber {
+	public static void main(String[] args) {
+		int a = 5;
+		System.out.println("Square: "+a*a);
+		System.out.println("Cube: "+a*a*a);
+	}
+}
