b72becc8dbf802858d14f57ad22672299991768d
saravana5632 <oatktg.saravana5632@gmail.com>
2026-09-26 00:29:48 +0530

    Added Extract Last digit

--- Lastdigit.java
+++ Lastdigit.java
@@ -1,6 +1,6 @@
 package numberproblems;
 import java.util.Scanner;
-public class Lastdigit {
+public class LastDigit {
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	int a=sc.nextInt();
